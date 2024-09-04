87ca4b6c9b0794e57f47a2d00c2884dcc6db533b
raveroses <odekunlewaris01@gmail.com>
2024-09-04 11:02:53 +0100

    My card validation project

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let cardName= document.querySelector(".cardName");
+let cardNumber= document.querySelector(".cardNumber")
+let firstSubFlexY= document.querySelector(".firstSubFlex input")
+let SecondSubFlexM= document.querySelector(".SecondSubFlex")
+let SecondFlex= document.querySelector(".SecondFlex input")
+
+
+
+let FirstContent= document.querySelector(".FirstContent span")
+let secondContentFlex= document.querySelector(".secondContentFlex span")
+let ThirdContent= document.querySelector(".ThirdContent")
+
